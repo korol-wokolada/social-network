3ce37b5b4f005a05afccae3c78d075bc0372f606
korol-wokolada <105501483+korol-wokolada@users.noreply.github.com>
2023-07-19 20:04:45 +0300

    add bottom scroll

--- src/Components/Messages/MessageArea/Message/Message.tsx
+++ src/Components/Messages/MessageArea/Message/Message.tsx
@@ -1,3 +1,28 @@
-export default function Message(props: any) {
-  return <div>{props.message}</div>;
+import { Avatar } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import style from "./Message.module.css";
+type Props = {
+  message: string;
+  photo?: string;
+  userName?: string;
+  userId?: number;
+};
+
+export default function Message({ message, photo, userId, userName }: Props) {
+  const avatar = photo ? (
+    <Avatar shape="circle" size={50} src={<img src={photo} alt="avatar" />} />
+  ) : (
+    <Avatar shape="circle" size={50} icon={<UserOutlined />} />
+  );
+
+  return (
+    <>
+      <div className={style.wrapper}>
+        <div>{avatar}</div>
+        <p>
+          {userName}: {message}
+        </p>
+      </div>
+    </>
+  );
 }
--- src/Components/Messages/MessageArea/MessageArea.tsx
+++ src/Components/Messages/MessageArea/MessageArea.tsx
@@ -4,7 +4,7 @@ import {
 } from "../../../Redux-Toolkit/Redux-Toolkit-Store";
 import style from "./MessagesArea.module.css";
 import { Input, Button } from "antd";
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 
 import Message from "./Message/Message";
 import {
@@ -17,16 +17,21 @@ const { TextArea } = Input;
 
 export function MessageArea() {
   const dispatch = useAppDispatch();
+  const messagesData = useAppSelector((state) => state.message.messages);
+  const [message, setMessage] = useState("");
+  const bottomRef = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    scrollToBottom();
+  });
+
   useEffect(() => {
     dispatch(startWebSocketChannel());
+
     return () => {
       dispatch(stopWebSocketChannel());
     };
   }, [dispatch]);
-
-  const messagesData = useAppSelector((state) => state.message.messages);
-  console.log(messagesData);
-  const [message, setMessage] = useState("");
 
   function sendMessage() {
     if (!message) {
@@ -41,13 +46,30 @@ export function MessageArea() {
 
   const messages = messagesData?.map(
     ({ message, photo, userId, userName }, index) => {
-      return <Message key={index} message={message} />;
+      return (
+        <Message
+          key={index + userId}
+          message={message}
+          photo={photo}
+          userName={userName}
+          userId={userId}
+        />
+      );
     }
   );
+  const scrollToBottom = () => {
+    bottomRef.current?.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  };
 
   return (
     <div className={style.wrapper}>
-      <div className={style.messageBlock}>{messages}</div>
+      <div className={style.messageBlock}>
+        {messages}
+        <div ref={bottomRef}></div>
+      </div>
       <div className={style.textArea}>
         <TextArea
           placeholder="Inter your email"
